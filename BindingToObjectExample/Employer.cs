9630602130a1fa1a3ed9bfe074d12612f0dc6978
Иванов Иван <zaazaa@yandex.ru>
2022-03-22 22:26:05 +0300

    Lesson6

---
+++ BindingToObjectExample/Employer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingToObjectExample
+{
+    public class Employer : INotifyPropertyChanged
+    {
+        private string name = "None";
+        private string description = "None";
+        private int age = 20;
+
+        public string Name { get => name; 
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+
+        public string Description { get => description; set => description = value; }
+        public int Age { get => age; set => age = value; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
