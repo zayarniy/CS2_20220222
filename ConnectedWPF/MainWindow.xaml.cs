b4f5d17ccd40fcb54d911887b8ad05f89c073a91
zaaza <zaaza@yandex.ru>
2022-03-25 23:30:59 +0300

    Lesson7

---
+++ ConnectedWPF/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConnectedWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection connection;
+        SqlDataAdapter adapter;
+        DataTable dt;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            //string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial catalog=SampleDatabase;Integrated Security=True;Connect Timeout=30";
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDBFilename=C:\Users\zaaza\source\repos\CS2_20220222\ConnectedWPF\bin\Debug\SampleDatabase.mdf;Integrated Security=True;Connect Timeout=30";
+            connection =new SqlConnection(connectionString);
+            adapter = new SqlDataAdapter();
+            SqlCommand command = new SqlCommand("SELECT ID,FIO, Birthday, Email, Phone From People", connection);
+            adapter.SelectCommand = command;
+            //Insert
+            command = new SqlCommand(@"INSERT INTO People (FIO, Birthday, Email, Phone) Values (@FIO, @Birthday, @Email, @Phone); SET @ID=@@IDENTITY;", connection);
+            command.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
+            command.Parameters.Add("@Birthday", SqlDbType.NVarChar, -1, "Birthday");
+            command.Parameters.Add("@Email", SqlDbType.NVarChar, 100, "Email");
+            command.Parameters.Add("@Phone", SqlDbType.NVarChar, -1, "Phone");
+            SqlParameter param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param.Direction = ParameterDirection.Output;
+            adapter.InsertCommand = command;
+            // update
+            command = new SqlCommand(@"UPDATE People SET FIO = @FIO, Birthday = @Birthday, Email = @Email, Phone = @Phone WHERE ID = @ID", connection);
+            command.Parameters.Add("@FIO", SqlDbType.NVarChar, -1, "FIO");
+            command.Parameters.Add("@Birthday", SqlDbType.NVarChar, -1, "Birthday");
+            command.Parameters.Add("@Email", SqlDbType.NVarChar, 100, "Email");
+            command.Parameters.Add("@Phone", SqlDbType.NVarChar, -1, "Phone");
+            param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param.SourceVersion = DataRowVersion.Original;
+            adapter.UpdateCommand = command;
+            //delete
+            command = new SqlCommand("DELETE FROM People WHERE ID = @ID", connection);
+            param = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            param.SourceVersion = DataRowVersion.Original;
+            adapter.DeleteCommand = command;
+            dt = new DataTable();
+            adapter.Fill(dt);
+            peopleDataGrid.DataContext = dt.DefaultView;
+
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            // добавим новую строку
+            DataRow newRow = dt.NewRow();
+            EditWindow editWindow = new EditWindow(newRow);
+            editWindow.ShowDialog();
+            if (editWindow.DialogResult.HasValue && editWindow.DialogResult.Value)
+            {
+                dt.Rows.Add(editWindow.resultRow);
+                adapter.Update(dt);
+            }
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView newRow = (DataRowView)peopleDataGrid.SelectedItem;
+            newRow.BeginEdit();
+            EditWindow editWindow = new EditWindow(newRow.Row);
+            editWindow.ShowDialog();
+            if (editWindow.DialogResult.HasValue && editWindow.DialogResult.Value)
+            {
+                newRow.EndEdit();
+                adapter.Update(dt);
+            }
+            else
+            {
+                newRow.CancelEdit();
+            }
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView newRow = (DataRowView)peopleDataGrid.SelectedItem;
+
+            newRow.Row.Delete();
+            adapter.Update(dt);
+        }
+    }
+}
---
+++ Disconnected01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AccessConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OleDbConnectionStringBuilder oleDbConnectionStringBuilder = new OleDbConnectionStringBuilder();
+
+            //Строка подключение
+            //string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Accounts;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=C:\Users\zaaza\source\repos\CS2_20220222\Disconnected01\bin\Debug\net6.0\Accounts.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //Объект подключение к базе данных
+            SqlConnection connection = new SqlConnection(connectionString);
+            //connection.ConnectionString = connectionString;
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Account", connection);
+                //SqlCommand command1 = new SqlCommand("Insert Into Account(nvcLogin,nvcPassword,bActive) Values ('Login','Password',1)",connection);
+                //SqlCommand command1 = new SqlCommand("Delete From Account Where nvcLogin='root'", connection);
+                SqlCommand command1 = new SqlCommand("Update Account Set nvcPassword='admin1' Where nvcLogin='admin'", connection);
+                int n=command1.ExecuteNonQuery();
+                SqlDataReader reader = command.ExecuteReader();
+                //OleDbDataReader reader = command.ExecuteScalar();
+                while (reader.Read())//Считываем каждую строчку
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}", (int)reader["id"], reader.GetString(1), reader.GetString(2), reader.GetBoolean(3));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection?.Close();
+            }
+            Console.ReadKey();
+        }
+    }
+}
